335e2dd9836c6d88a95ac360d09d879ded0274c4
Rémi Garcia <remi.garcia.pro@gmail.com>
2015-08-01 00:10:04 +0200

    added saveAlias and saveMailbox methods to service

--- Service/MysqlPostfixAdminService.php
+++ Service/MysqlPostfixAdminService.php
@@ -3,7 +3,9 @@
 namespace TroisSix\MysqlPostfixAdminBundle\Service;
 
 use Doctrine\ORM\EntityManager;
+use TroisSix\MysqlPostfixAdminBundle\Entity\Alias;
 use TroisSix\MysqlPostfixAdminBundle\Entity\Domain;
+use TroisSix\MysqlPostfixAdminBundle\Entity\Mailbox;
 
 class MysqlPostfixAdminService
 {
@@ -27,5 +29,23 @@ class MysqlPostfixAdminService
     {
         $this->em->persist($domain);
         $this->em->flush($domain);
+    }
+
+    /**
+     * @param Alias $alias
+     */
+    public function saveAlias(Alias $alias)
+    {
+        $this->em->persist($alias);
+        $this->em->flush($alias);
+    }
+
+    /**
+     * @param Mailbox $mailbox
+     */
+    public function saveMailbox(Mailbox $mailbox)
+    {
+        $this->em->persist($mailbox);
+        $this->em->flush($mailbox);
     }
 }
